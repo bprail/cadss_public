80326a47c17691cb139e898b2e68da131eb09760
Swamynathan Siva <swamy.august@outlook.in>
2024-04-21 08:50:08 -0400

    Cpp is working

--- interconnect/interconnect.c
+++ interconnect/interconnect.c
@@ -5,61 +5,10 @@
 #include <interconnect_internal.h>
 
 
+interconn * self_c;
+
 void busReq(bus_req_type brt, uint64_t addr, int procNum){
-    if (pendingRequest == NULL)
-    {
-        assert(brt != SHARED);
-
-        bus_req* nextReq = calloc(1, sizeof(bus_req));
-        nextReq->brt = brt;
-        nextReq->currentState = WAITING_CACHE;
-        nextReq->addr = addr;
-        nextReq->procNum = procNum;
-        nextReq->dataAvail = 0;
-
-        pendingRequest = nextReq;
-        countDown = CACHE_DELAY;
-        if (CADSS_VERBOSE && PENDING_REQUEST_DEBUG)
-            fprintf(stdout, "new pendingRequest(proc=%d) from busReq\n", pendingRequest->procNum);
-
-
-    } else if (brt == SHARED && pendingRequest->addr == addr) {
-        pendingRequest->shared = 1;
-
-    } else if (brt == DATA && pendingRequest->addr == addr) {   
-        assert(pendingRequest->currentState == WAITING_MEMORY ||
-               pendingRequest->currentState == TRANSFERING_CACHE ||
-               pendingRequest->currentState == TRANSFERING_MEMORY);
-
-        if (pendingRequest->currentState == WAITING_MEMORY) {
-            // This DATA busReq addresses the current pending request
-            pendingRequest->data = 1;
-            pendingRequest->currentState = TRANSFERING_CACHE;
-            countDown = CACHE_TRANSFER;
-            return;
-        } else if (CADSS_VERBOSE && PENDING_REQUEST_DEBUG) {
-            // pendingRequest was already served by another proc's DATA busReq
-            // Occurs because all sharing procs snoop a READ(X), and send data.
-            // With correct directory/arbitration, this should not trigger
-            fprintf(stdout, "Duplicate DATA busReq received from proc , ignoring.\n");
-        }
-    } else {
-        assert(brt != SHARED);
-
-        bus_req* nextReq = calloc(1, sizeof(bus_req));
-        nextReq->brt = brt;
-        nextReq->currentState = QUEUED;
-        nextReq->addr = addr;
-        nextReq->procNum = procNum;
-        nextReq->dataAvail = 0;
-
-        enqBusRequest(nextReq, procNum);
-    }
-}
-
-interconn* init(inter_sim_args* isa)
-{
-    return init_cpp(isa);
+    busReq_cpp(brt, addr, procNum);
 }
 
 
@@ -87,4 +36,18 @@ int busReqCacheTransfer(uint64_t addr, int procNum)
  int destroy(void)
 {
     return destroy_cpp();
+}
+
+interconn* init(inter_sim_args* isa)
+{
+    self_c = malloc(sizeof(interconn));
+    self_c->busReq = busReq;
+    self_c->registerCoher = registerCoher;
+    self_c->busReqCacheTransfer = busReqCacheTransfer;
+    self_c->si.tick = tick;
+    self_c->si.finish = finish;
+    self_c->si.destroy = destroy;
+    
+    init_cpp(isa,  self_c);
+    return self_c;
 }
